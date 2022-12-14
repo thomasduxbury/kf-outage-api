26a753bddc11ac437497a20e263035ee9c915622
Thomas Duxbury <thomas.duxbury90@gmail.com>
2022-12-14 21:04:23 +0000

    Initial commit

---
+++ src/main/java/com/krakenflex/outageapi/OutageApiApplication.java
@@ -0,0 +1,13 @@
+package com.krakenflex.outageapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OutageApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OutageApiApplication.class, args);
+	}
+
+}
